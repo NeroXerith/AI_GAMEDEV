41260fb6901abd4872653f9f566ca42db2535957
NeroXerith <biene.bryle.sanico@adamson.edu.ph>
2023-05-26 22:16:57 +0800

    Fixed image renderings warnings

--- src/components/UI/GameUI.js
+++ src/components/UI/GameUI.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import { Button, Box, Container, Typography, Paper } from '@mui/material';
+import rockImage from '../assets/rock.svg';
+import paperImage from '../assets/paper.svg';
+import scissorsImage from '../assets/scissors.svg';
+import vsImage from '../assets/vs.svg';
+
+const images = {
+  rock: rockImage,
+  paper: paperImage,
+  scissors: scissorsImage,
+};
 
 const GameUI = ({ choices, handlePlayerChoice, playerChoice, computerChoice, result }) => {
   return (
@@ -15,15 +25,15 @@ const GameUI = ({ choices, handlePlayerChoice, playerChoice, computerChoice, res
         <Box sx={{ display: 'flex', flexDirection: 'row' }}>
           <Box elevation={3} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)', height: '100%', width: '60%' }}>
             <Typography variant="h2">Player (YOU):</Typography>
-            <img src={`../assets/${playerChoice}.svg`} height="100%" width="100%" alt="actionImage" />
+            <img src={images[playerChoice]} height="100%" width="100%" alt="actionImage" />
             <Typography variant="h4">{playerChoice}</Typography>
           </Box>
           <Box sx={{ marginLeft: '15px' }}>
-            <img src="../assets/vs.svg" height="90%" width="90%" alt="vs" />
+            <img src={vsImage} height="90%" width="90%" alt="vs" />
           </Box>
           <Box elevation={3} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)', height: '100%', width: '60%' }}>
             <Typography variant="h2">Computer:</Typography>
-            <img src={`../assets/${computerChoice}.svg`} height="100%" width="100%" style={{ transform: 'scaleX(-1)' }} alt="actionImage" />
+            <img src={images[computerChoice]} height="100%" width="100%" style={{ transform: 'scaleX(-1)' }} alt="actionImage" />
             <Typography variant="h4">{computerChoice}</Typography>
           </Box>
         </Box>
